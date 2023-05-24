a03e6099064546c305be979ae0e8251715bb2409
Charles Zhang <charleszhang99@yahoo.com>
2023-05-25 01:47:48 +0200

    ppc: Use one sap.lisp.

--- src/compiler/ppc/sap.lisp
+++ src/compiler/ppc/sap.lisp
@@ -137,6 +137,50 @@
 (macrolet ((def-system-ref-and-set
                (ref-name set-name sc type size &optional signed)
              `(progn
+                ;; smaller-than-word atomics are not necessarily implemented in hardware.
+                ;; I found something about it regarding C compiler intrinsics:
+                ;; "__lqarx, __lharx, and __lbarx is valid only when -qarch is set to target POWER8 processors."
+                ;; https://www.ibm.com/docs/en/xl-c-aix/13.1.0?topic=functions-lqarx-ldarx-lwarx-lharx-lbarx
+                ;; and in fact they're not present on gcc110.fsffrance.org
+                ;; You're supposed to use lwarx, stwcx. in those cases,
+                ;; which I don't feel like doing.
+                #+64-bit
+                ,@(when (member ref-name '(sap-ref-8 sap-ref-16
+                                           sap-ref-32 sap-ref-64 signed-sap-ref-64
+                                           sap-ref-lispobj sap-ref-sap))
+                    (multiple-value-bind (load store)
+                        (ecase size
+                          (:byte   (values 'lbarx 'stbcx.))
+                          (:short  (values 'lharx 'sthcx.))
+                          (:word   (values 'lwarx 'stwcx.))
+                          (:dword  (values 'ldarx 'stdcx.)))
+                      `((define-vop (,(symbolicate "CAS-" ref-name))
+                          (:translate (cas ,ref-name))
+                          (:policy :fast-safe)
+                          (:args (oldval :scs (,sc))
+                                 (newval :scs (,sc))
+                                 (sap :scs (sap-reg))
+                                 (offset :scs (signed-reg)))
+                          (:arg-types ,type ,type system-area-pointer signed-num)
+                          (:results (result :scs (,sc) :from :load))
+                          (:result-types ,type)
+                          (:generator 5
+                            ;; sap-registers = non-descriptor-registers which does not include reg 0,
+                            ;; but make sure, because 0 as RA means the immediate value 0.
+                            (aver (/= (tn-offset sap) 0))
+                            (inst sync)
+                            LOOP
+                            (inst ,load result sap offset)
+                            ;; The load-and-reserve forms shorter than 'Doubleword' clear
+                            ;; all the remaining bits of the destination register,
+                            ;; so it's always OK to use a doubleword comparison.
+                            (inst cmpd result oldval)
+                            (inst bne EXIT)
+                            (inst ,store newval sap offset)
+                            (inst bne LOOP)
+                            EXIT
+                            (inst isync))))))
+
                    (define-vop (,ref-name)
                      (:translate ,ref-name)
                      (:policy :fast-safe)
@@ -149,12 +193,16 @@
                       (inst ,(ecase size
                                     (:byte 'lbzx)
                                     (:short (if signed 'lhax 'lhzx))
-                                    (:long 'lwzx)
+                                    (:word
+                                     #-64-bit 'lwzx
+                                     #+64-bit (if signed 'lwax 'lwzx))
+                                    (:dword 'ldx)
                                     (:single 'lfsx)
                                     (:double 'lfdx))
                             result sap offset)
                       ,@(when (and (eq size :byte) signed)
                               '((inst extsb result result)))))
+                   #-64-bit ; FIXME: need to add a constraint on the offset alignment for dword.
                    (define-vop (,(symbolicate ref-name "-C"))
                      (:translate ,ref-name)
                      (:policy :fast-safe)
@@ -167,7 +215,7 @@
                       (inst ,(ecase size
                                     (:byte 'lbz)
                                     (:short (if signed 'lha 'lhz))
-                                    (:long 'lwz)
+                                    (:word 'lwz)
                                     (:single 'lfs)
                                     (:double 'lfd))
                             result sap offset)
@@ -184,10 +232,12 @@
                       (inst ,(ecase size
                                     (:byte 'stbx)
                                     (:short 'sthx)
-                                    (:long 'stwx)
+                                    (:word 'stwx)
+                                    (:dword 'stdx)
                                     (:single 'stfsx)
                                     (:double 'stfdx))
                             value sap offset)))
+                   #-64-bit ; FIXME: need to add a constraint on the offset alignment for dword.
                    (define-vop (,(symbolicate set-name "-C"))
                      (:translate ,set-name)
                      (:policy :fast-safe)
@@ -199,7 +249,7 @@
                       (inst ,(ecase size
                                     (:byte 'stb)
                                     (:short 'sth)
-                                    (:long 'stw)
+                                    (:word 'stw)
                                     (:single 'stfs)
                                     (:double 'stfd))
                             value sap offset))))))
@@ -212,13 +262,19 @@
   (def-system-ref-and-set signed-sap-ref-16 %set-signed-sap-ref-16
     signed-reg tagged-num :short t)
   (def-system-ref-and-set sap-ref-32 %set-sap-ref-32
-    unsigned-reg unsigned-num :long nil)
+    unsigned-reg unsigned-num :word nil)
   (def-system-ref-and-set signed-sap-ref-32 %set-signed-sap-ref-32
-    signed-reg signed-num :long t)
+    signed-reg signed-num :word t)
+  #+64-bit
+  (def-system-ref-and-set sap-ref-64 %set-sap-ref-64
+    unsigned-reg unsigned-num :dword)
+  #+64-bit
+  (def-system-ref-and-set signed-sap-ref-64 %set-signed-sap-ref-64
+    signed-reg signed-num :dword t)
   (def-system-ref-and-set sap-ref-sap %set-sap-ref-sap
-    sap-reg system-area-pointer :long)
+    sap-reg system-area-pointer #-64-bit :word #+64-bit :dword)
   (def-system-ref-and-set sap-ref-lispobj %set-sap-ref-lispobj
-    descriptor-reg * :long)
+    descriptor-reg * #-64-bit :word #+64-bit :dword)
   (def-system-ref-and-set sap-ref-single %set-sap-ref-single
     single-reg single-float :single)
   (def-system-ref-and-set sap-ref-double %set-sap-ref-double
