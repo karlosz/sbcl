64b9045bc0563b8b9a5a5da47bebd58b095e2cf0
Charles Zhang <charleszhang99@yahoo.com>
2024-07-17 16:56:19 +0200

    Indirect DEBUG-INFO accesses through a hash table on #+pie-for-elf.
    This allows us to grab at debug info stuff in a PIE elfinated core,
    which allows us to disassemble things and access function metadata
    such as names correctly on the Lisp side.

--- src/compiler/generic/objdef.lisp
+++ src/compiler/generic/objdef.lisp
@@ -206,7 +206,7 @@ during backtrace.
   ;; the slot index; and there's a special variant if #+darwin-jit.
   (debug-info :type t
               :ref-known (flushable)
-              :ref-trans %code-debug-info)
+              :ref-trans #-pie-for-elf %code-debug-info #+pie-for-elf %%code-debug-info)
   (constants :rest-p t))
 
 (define-primitive-object (fdefn :type fdefn
