664b0349b54d134eafce3aa34ef6c3bcacdcdaea
Charles Zhang <charleszhang99@yahoo.com>
2024-07-18 11:29:46 +0200

    Randomize startup space addresses with PIECORE.
    The point of PIE elfination is so that we don't reference any absolute
    addresses in the text sections, an assumption we can enforce by not
    asking mmap to map spaces to a particular address, in effect
    randomizing the startup space addresses.

--- src/runtime/coreparse.c
+++ src/runtime/coreparse.c
@@ -856,7 +856,7 @@ process_directory(int count, struct ndir_entry *entry,
             int sub_2gb_flag = (request & 1);
             request &= ~(size_t)1;
             // Try to map at address requested by the core file unless ASLR.
-#ifdef LISP_FEATURE_ASLR
+#if defined(LISP_FEATURE_ASLR) || defined PIECORE
             addr = 0;
 #endif
             addr = (uword_t)reserve_space(id, sub_2gb_flag ? MOVABLE_LOW : MOVABLE,
